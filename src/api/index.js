5001e9bef0324d40324f688a6f140c3a5f28258a
gabriel-miranda <gabmirdev@gmail.com>
2017-12-11 23:16:48 -0300

    Removed unused and duplicated api code

--- src/api/index.js
+++ src/api/index.js
@@ -3,24 +3,23 @@ import { graphqlFastify, graphiqlFastify } from 'apollo-server-fastify';
 import { connect, collections } from './db';
 import schema from './graphql';
 
+const GRAPH_QL_ROUTE = '/graphql';
+const GRAPH_QL_ENDPOINT = context => graphqlFastify({ schema, context });
+
 (async () => {
   try {
     const db = await connect();
 
     const app = fastify();
 
-    const context = collections(db);
+    const ctx = collections(db);
 
-    app.get('/', (req, res) => res.send({ hello: 'world' }));
+    app.post(GRAPH_QL_ROUTE, GRAPH_QL_ENDPOINT(ctx));
 
-    app.get('/data', (req, res) => res.send({ data: 'Data from the server' }));
-
-    app.post('/graphql', graphqlFastify({ schema, context }));
-
-    app.get('/graphql', graphqlFastify({ schema, context }));
+    app.get(GRAPH_QL_ROUTE, GRAPH_QL_ENDPOINT(ctx));
 
     app.get('/graphiql', graphiqlFastify({
-      endpointURL: '/graphql',
+      endpointURL: GRAPH_QL_ROUTE,
     }));
 
     app.listen(3001, (err) => {
